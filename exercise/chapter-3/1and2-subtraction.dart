2f8c9079ba22226629859d02246ce372e386adf0
Felipe <ofelipe439@gmail.com>
2025-11-10 08:03:33 -0300

    corrected exercise 1

--- exercise/chapter-3/1and2-subtraction.dart
+++ exercise/chapter-3/1and2-subtraction.dart
@@ -1,20 +1,20 @@
 import 'dart:io';
-
-import '../reads.dart';
+import '../reads.dart'; 
 
 void main() {
   double firstNumber = readDouble("write the first number: ");
   double secondNumber = readDouble("write the second number: ");
-  double? thirdNumber;
+  double? thirdNumber; 
   double result;
 
-  readDouble("Do you want to write a third number? (Y or N) ");
+  print("Do you want to write a third number? (Y or N) "); 
+
   if (stdin.readLineSync()?.toUpperCase() == 'Y') {
-    double thirdNumber = readDouble("write the third number: ");
+    thirdNumber = readDouble("write the third number: ");
   }
 
   if (thirdNumber != null) {
-    result = firstNumber - secondNumber - thirdNumber;
+    result = firstNumber - secondNumber - thirdNumber; 
     print(
       "The result of subtracting $secondNumber and $thirdNumber from $firstNumber is: $result",
     );
